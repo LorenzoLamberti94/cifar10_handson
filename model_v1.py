0e1ccdec0caeee9aa76eff692865c6fc9aebba26
lorenzo.lamberti <lorenzo.lamberti@unibo.it>
2022-12-01 13:34:09 +0000

    removed code for exercises

--- model_v1.py
+++ model_v1.py
@@ -47,24 +47,24 @@ def model_v2():
 	model = Sequential()
 
 	model.add(conv_bn_relu_layer(16, (5,5), padding='same', input_shape=(32,32,3)))
-	model.add(conv_bn_relu_layer(32, (7,7), padding='same', strides=(2,2)))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (7,7), padding='same', strides=(2,2)))
 	model.add(layers.Dropout(0.3))
 
-	model.add(conv_bn_relu_layer(48, (5,5), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (5,5), padding='same'))
 	model.add(layers.MaxPooling2D(pool_size=(2,2)))
-	model.add(conv_bn_relu_layer(64, (5,5), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (5,5), padding='same'))
 	model.add(layers.Dropout(0.5))
 
-	model.add(conv_bn_relu_layer(128, (3,3), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (3,3), padding='same'))
 	model.add(layers.MaxPooling2D(pool_size=(2,2)))
-	model.add(conv_bn_relu_layer(128, (3,3), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (3,3), padding='same'))
 	model.add(layers.Dropout(0.5))
 
-	model.add(conv_bn_relu_layer(64, (3,3), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (3,3), padding='same'))
 	model.add(layers.Dropout(0.5))
 
 	model.add(layers.Flatten())
-	model.add(layers.Dense(128))
+	model.add(layers.Dense("""YOUR_CODE_HERE"""))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
 	model.add(layers.Dropout(0.5))
@@ -75,78 +75,75 @@ def model_v3():
 	model = Sequential()
 
 	model.add(conv_bn_relu_layer(16, (5,5), padding='same', input_shape=(32,32,3)))
-	model.add(conv_bn_relu_layer(32, (5,5), padding='same', strides=(2,2)))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", ("""YOUR_CODE_HERE"""), padding='same', strides=(2,2)))
 	model.add(layers.Dropout(0.3))
 
-	model.add(conv_bn_relu_layer(48, (3,3), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", ("""YOUR_CODE_HERE"""), padding='same'))
 	model.add(layers.MaxPooling2D(pool_size=(2,2)))
-	model.add(conv_bn_relu_layer(64, (3,3), padding='same', strides=(2,2)))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", ("""YOUR_CODE_HERE"""), padding='same', strides=(2,2)))
 	model.add(layers.Dropout(0.5))
 
-	model.add(conv_bn_relu_layer(128, (3,3), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (3,3), padding='same'))
 	model.add(layers.MaxPooling2D(pool_size=(2,2)))
-	model.add(conv_bn_relu_layer(128, (1,1), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (1,1), padding='same'))
 	model.add(layers.Dropout(0.5))
 
-	model.add(conv_bn_relu_layer(64, (3,3), padding='same'))
+	model.add(conv_bn_relu_layer("""YOUR_CODE_HERE""", (3,3), padding='same'))
 	model.add(layers.Dropout(0.5))
 
 	model.add(layers.Flatten())
-	model.add(layers.Dense(128))
+	model.add(layers.Dense("""YOUR_CODE_HERE"""))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
 	model.add(layers.Dropout(0.5))
 	model.add(layers.Dense(10, activation='softmax'))    # num_classes = 10
 	return model
 
-def model_v4():
-	model = Sequential()
-	return model
-	
+
 # Sequential layers are not supported by TensorFlow's QAT
 # Model 5 is similar to Model 4, without nested Sequential
-def model_v5():
+def model_v4():
 	model = Sequential()
 
 	model.add(layers.Conv2D(16, (5,5), padding='same', input_shape=(32,32,3)))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
 
-	model.add(layers.DepthwiseConv2D((3,3), padding='same', strides=(2,2)))
+	model.add(layers.DepthwiseConv2D(("""YOUR_CODE_HERE"""), padding='same', strides=(2,2)))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-	model.add(layers.Conv2D(32, (1,1)))
+	model.add(layers.Conv2D(32, ("""YOUR_CODE_HERE""")))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
 	model.add(layers.Dropout(0.3))
 
-	model.add(layers.DepthwiseConv2D((3,3), padding='same'))
+	model.add(layers.DepthwiseConv2D(("""YOUR_CODE_HERE"""), padding='same'))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-	model.add(layers.Conv2D(48, (1,1)))
+	model.add(layers.Conv2D(48, ("""YOUR_CODE_HERE""")))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
 	
-	model.add(layers.DepthwiseConv2D((3,3), padding='same',strides=(2,2)))
+	model.add(layers.DepthwiseConv2D(("""YOUR_CODE_HERE"""), padding='same',strides=(2,2)))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-	model.add(layers.Conv2D(64, (1,1)))
+	model.add(layers.Conv2D(64, ("""YOUR_CODE_HERE""")))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
 	
 	model.add(layers.Dropout(0.5))
 	
-	model.add(layers.DepthwiseConv2D((3,3), padding='same',strides=(2,2)))
+	model.add(layers.DepthwiseConv2D(("""YOUR_CODE_HERE"""), padding='same',strides=(2,2)))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-	model.add(layers.Conv2D(128, (1,1)))
+	model.add(layers.Conv2D(128, ("""YOUR_CODE_HERE""")))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
 	
-	model.add(layers.DepthwiseConv2D((3,3), padding='same'))
+	model.add(layers.DepthwiseConv2D(("""YOUR_CODE_HERE"""), padding='same'))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
-	model.add(layers.Conv2D(128, (1,1)))
+	model.add(layers.Conv2D(128, ("""YOUR_CODE_HERE""")))
 	model.add(layers.BatchNormalization())
 	model.add(layers.ReLU())
 
@@ -160,3 +157,8 @@ def model_v5():
 	model.add(layers.Dense(10, activation='softmax'))    # num_classes = 10
 	return model
 
+
+def model_v5():
+	model = Sequential()
+	return model
+	
